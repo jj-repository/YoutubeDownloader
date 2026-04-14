224d15c978b7766dd7c661f454aa00c14d0a6aad
jj-repository <jj-repository@users.noreply.github.com>
2026-04-14 20:36:21 +0200

    bump version to 5.23

--- constants.py
+++ constants.py
@@ -71,7 +71,7 @@ MAX_CLIPBOARD_URLS = 500
 CLIPBOARD_URL_LIST_HEIGHT = 150
 
 # Version and Update
-APP_VERSION = "5.22"
+APP_VERSION = "5.23"
 GITHUB_REPO = "jj-repository/YoutubeDownloader"
 GITHUB_RELEASES_URL = f"https://github.com/{GITHUB_REPO}/releases"
 GITHUB_API_LATEST = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
